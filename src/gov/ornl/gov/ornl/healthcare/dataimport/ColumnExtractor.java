2c11ef8f55dfca98165a93f60555e98ca9dcdd1a
Varun Chandola <6qz@ornl.gov>
2013-07-25 09:28:36 -0400

    Rule class added

--- src/gov/ornl/gov/ornl/healthcare/dataimport/ColumnExtractor.java
+++ src/gov/ornl/gov/ornl/healthcare/dataimport/ColumnExtractor.java
@@ -6,10 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Connection;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 /*
  * Simple utility to connect from postgres to Mongo
@@ -17,21 +20,21 @@ import com.mongodb.MongoClient;
  */
 public class ColumnExtractor
 {
-  //default connection properties
+	//default connection properties
 	String pg_url = "jdbc:postgresql://localhost/healthcare";
 	String pg_user = "postgres";
 	String pg_password = "postgres";
 	String pg_table = "nppes";
-	
+
 	Connection pg_con = null;
 	Statement pg_st = null;
 	ResultSet pg_rs = null;
 
 	MongoClient mongo_client = null;
 	DB mongo_db = null;
-	
-	Logger log = Logger.getLogger(ColumnExtractor.class.getName());
-	
+
+	private final static Logger log = Logger.getLogger(ColumnExtractor.class.getName());
+
 	public void pg_initDB()
 	{
 		try
@@ -43,7 +46,7 @@ public class ColumnExtractor
 			log.log(Level.SEVERE,e.getMessage(),e);
 		}
 	}
-	
+
 	public void pg_closeDB()
 	{
 		try
@@ -60,7 +63,7 @@ public class ColumnExtractor
 			log.log(Level.WARNING, e.getMessage(), e);
 		}
 	}
-	
+
 	public void mongo_initDB()
 	{
 		try
@@ -71,37 +74,72 @@ public class ColumnExtractor
 			log.log(Level.SEVERE, e.getMessage(),e);
 			return;
 		}
-		mongo_db = mongo_client.getDB( "healthcare" );
+		mongo_db = mongo_client.getDB( "healthcare" );		
 	}
-	
+
 	public void mongo_closeDB()
 	{
 		if(mongo_client != null)
 			mongo_client.close();
 	}
-	
-	public void extractColumn(String columnName)
+
+	public void extractColumn(Vector<String> fieldNames)
 	{
 		try
 		{
+			//extract column from Postgres
+			ExtractionRule rule = new ExtractionRule(ExtractionRule.TYPE.COMPOUND);
+			rule.setFieldNames(fieldNames);
 			pg_st = pg_con.createStatement();
-			pg_rs = pg_st.executeQuery("SELECT DISTINCT("+columnName+") FROM "+pg_table);
-			if(pg_rs.next())
-				System.out.println(pg_rs.getString(1));
+			pg_rs = pg_st.executeQuery("SELECT DISTINCT("+rule.getQuery()+") FROM "+pg_table);
 		}
 		catch(SQLException e)
 		{
 			log.log(Level.WARNING, e.getMessage(),e);
+		}		
+
+	}
+
+	public void migrateColumn(String collectionName)
+	{
+		if(pg_rs != null)
+		{
+			try
+			{
+				//choose the Mongo Collection to add to
+				DBCollection collection = mongo_db.getCollection(collectionName);
+				while(pg_rs.next())
+				{
+					BasicDBObject obj = new BasicDBObject("text", pg_rs.getString(1));
+					collection.insert(obj);
+				}
+
+			}
+			catch(SQLException e)
+			{
+				log.log(Level.WARNING, e.getMessage(),e);
+			}
 		}
 	}
-	
+
 	public static void main(String [] args)
 	{
+		log.setLevel(Level.INFO);
+		long start,end;
 		ColumnExtractor extractor = new ColumnExtractor();
 		extractor.pg_initDB();
 		extractor.mongo_initDB();
-		String columnName = "NPI";
-		extractor.extractColumn(columnName);
+		Vector<String> fieldNames = new Vector<String>(2);
+		fieldNames.add("provider_first_name");
+		fieldNames.add("provider_last_name");
+		start = System.currentTimeMillis();
+		extractor.extractColumn(fieldNames);
+		end = System.currentTimeMillis();
+		log.log(Level.INFO,"Column extracted in "+(end - start)+" milliseconds.");
+		start = System.currentTimeMillis();
+		extractor.migrateColumn("provider_names");
+		end = System.currentTimeMillis();
+		log.log(Level.INFO,"Column migrated in "+(end - start)+" milliseconds.");
 		extractor.pg_closeDB();
 		extractor.mongo_closeDB();
 	}
---
+++ src/gov/ornl/gov/ornl/healthcare/dataimport/ExtractionRule.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package gov.ornl.healthcare.dataimport;
+
+import java.util.Vector;
+
+/**
+ * Wrapper class for encoding rules to combine fieldnames
+ * @author chandola
+ *
+ */
+public class ExtractionRule
+{
+	public static enum TYPE
+	{
+		SINGLE, COMPOUND
+	};
+	private ExtractionRule.TYPE ruleType;
+	private Vector<String> fieldNames;
+	
+	public ExtractionRule(ExtractionRule.TYPE type)
+	{
+		this.ruleType = type;
+		this.fieldNames = new Vector<String>();
+	}
+	
+	public void setFieldNames(Vector<String> fieldNames)
+	{
+		this.fieldNames = fieldNames;
+	}
+	
+	public void append(String fieldName)
+	{
+		this.fieldNames.add(fieldName);
+	}
+	
+	public String getQuery()
+	{
+		if(ruleType == ExtractionRule.TYPE.SINGLE)
+			return fieldNames.firstElement();
+		if(fieldNames.size() == 0)
+			return "";
+		
+		StringBuffer buf = new StringBuffer();
+		for(int i = 0; i < fieldNames.size() - 1; i++)
+		{
+			buf.append("trim(");
+			buf.append(fieldNames.get(i));
+			buf.append(")||' '||");
+		}
+		buf.append("trim(");
+		buf.append(fieldNames.get(fieldNames.size() - 1));
+		buf.append(")");
+		return buf.toString();
+	}
+}
